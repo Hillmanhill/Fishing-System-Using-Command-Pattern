cdc48eff8629de235a1981b6589de62c5843d847
Hillmanhill <harveyhill61@gmail.com>
2025-09-17 19:52:38 +0100

    minor character control fixes and test scene set up

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -54,4 +54,4 @@ func PlayerMover() -> void:
 		velocity.z = move_toward(velocity.z, 0, RELETIVESPEED)
 	
 	if Input.is_action_just_pressed("PlayerJump") and is_on_floor():
-		velocity.y = 9
+		velocity.y = 20
